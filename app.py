63f152663d3085c5201ab5153fa0f3a3405204cb
adnope <anhduyxh7@gmail.com>
2024-12-11 07:43:45 +0700

    Implemented login and register site

--- app.py
+++ app.py
@@ -3,18 +3,16 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-# MySQL Configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'mywebsitedb'
 
-app.secret_key = 'nguyenanhduydeptraivocung'
+app.secret_key = 'ThrillOfTheHunt'
 
 mysql = MySQL(app)
 
-# Registration Page (Root URL)
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -43,37 +41,14 @@ def login():
         if user:
             session['user_id'] = user[0]
             flash('Login successful!', 'success')
-            return redirect('/home')
+            return redirect('/')
         else:
             flash('Invalid email or password.', 'danger')
     return render_template('login.html')
 
-@app.route('/users')
-def index():
-    # Fetch users from MySQL
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users")
-    users = cur.fetchall()
-    cur.close()
-    return render_template('index.html', users=users)
-
-@app.route('/add', methods=['GET', 'POST'])
-def add_user():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        # Insert into MySQL
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users (name, email) VALUES (%s, %s)", (name, email))
-        mysql.connection.commit()
-        cur.close()
-        return redirect('/')
-    return render_template('add_user.html')
-
-@app.route('/home')
+@app.route('/')
 def home():
-    
     return render_template('homepage.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
